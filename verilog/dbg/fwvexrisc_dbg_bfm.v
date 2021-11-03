964a46df73e17c5a88b381411fd3dafad2217593
Matthew Ballance <matt.ballance@gmail.com>
2021-11-03 10:11:13 -0700

    Remove leftover code
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- verilog/dbg/fwvexrisc_dbg_bfm.v
+++ verilog/dbg/fwvexrisc_dbg_bfm.v
@@ -31,46 +31,6 @@ module fwvexrisc_dbg_bfm(
 		input     [31:0]   rvfi_mem_rdata,
 		input     [31:0]   rvfi_mem_wdata);
 
-`ifdef UNDEFINED
-	wire 				rv_dbg_valid     = ivalid;
-	wire[31:0] 			rv_dbg_instr     = instr;
-	wire				rv_dbg_trap      = trap; 
-	wire				rv_dbg_tret      = tret; 
-	reg[4:0] 			rv_dbg_rd_addr   = 0; 
-	reg[31:0] 			rv_dbg_rd_wdata  = 0;
-	wire[31:0]			rv_dbg_pc        = pc;
-	reg[31:0]			rv_dbg_mem_addr  = {32{1'b0}};
-	reg[3:0]			rv_dbg_mem_wmask = {4{1'b0}};
-	reg[3:0]			rv_dbg_mem_rmask = {4{1'b0}};
-	reg[31:0]			rv_dbg_mem_data  = {32{1'b0}};
-	
-	always @(posedge clock) begin
-		if (rv_dbg_valid) begin
-			rv_dbg_mem_wmask <= {4{1'b0}};
-			rv_dbg_mem_rmask <= {4{1'b0}};
-			rv_dbg_mem_addr  <= {32{1'b0}};
-			rv_dbg_mem_data <= {32{1'b0}};
-			rv_dbg_rd_addr <= 0;
-			rv_dbg_rd_wdata <= 0;
-		end else begin
-			if (mvalid) begin
-				rv_dbg_mem_addr  <= maddr;
-				if (mwrite) begin
-					rv_dbg_mem_wmask <= mstrb;
-					rv_dbg_mem_data <= mdata;
-				end else begin
-					rv_dbg_mem_rmask <= mstrb;
-					rv_dbg_mem_data <= mdata;
-				end
-			end
-			if (rd_write) begin
-				rv_dbg_rd_addr <= rd_waddr;
-				rv_dbg_rd_wdata <= rd_wdata;
-			end
-		end
-	end	
-`endif
-
 	riscv_debug_bfm u_dbg (
 			.clock(				clock),
 			.reset(				reset),
